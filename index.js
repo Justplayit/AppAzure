8488ec0525f5dca6d0077279d397448313c6112e
Mihai Corciu <free_roaming94@yahoo.com>
2017-05-21 17:52:42 +0300

    modified:   index.js

--- index.js
+++ index.js
@@ -3,25 +3,25 @@ var fs = require('fs')
 
 var server = http.createServer(function(request, response) {
 
-    // response.writeHead(200, {"Content-Type": "text/plain"});
-    // response.end("Hello Azure!");
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello Azure!");
 
-    var pathname = url.parse(request.url).pathname;
-
-    console.log('Request for ' + pathname + ' received.');
-
-    fs.readFile(pathname.substr(1), function (err, data) {
-        if(err){
-            console.log(err);
-            response.writeHead(404, {'Content-Type': 'text/html'});
-        }
-        else {
-            response.writeHead(200, {'Content-Type': 'text/htl'});
-            response.write(data.toString());
-
-        }
-        response.end();
-    });
+    // var pathname = url.parse(request.url).pathname;
+    //
+    // console.log('Request for ' + pathname + ' received.');
+    //
+    // fs.readFile(pathname.substr(1), function (err, data) {
+    //     if(err){
+    //         console.log(err);
+    //         response.writeHead(404, {'Content-Type': 'text/html'});
+    //     }
+    //     else {
+    //         response.writeHead(200, {'Content-Type': 'text/htl'});
+    //         response.write(data.toString());
+    //
+    //     }
+    //     response.end();
+    // });
 
 });
 
