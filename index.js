fe03ca2b5acd81d8d003160a08be8a89c3b8a747
Mihai Corciu <free_roaming94@yahoo.com>
2017-05-21 18:11:15 +0300

    modified:   .idea/workspace.xml 	modified:   index.js 	renamed:    webpage/bootstrap/css/bootstrap-theme.css -> public/bootstrap/css/bootstrap-theme.css 	renamed:    webpage/bootstrap/css/bootstrap-theme.css.map -> public/bootstrap/css/bootstrap-theme.css.map 	renamed:    webpage/bootstrap/css/bootstrap-theme.min.css -> public/bootstrap/css/bootstrap-theme.min.css 	renamed:    webpage/bootstrap/css/bootstrap-theme.min.css.map -> public/bootstrap/css/bootstrap-theme.min.css.map 	renamed:    webpage/bootstrap/css/bootstrap.css -> public/bootstrap/css/bootstrap.css 	renamed:    webpage/bootstrap/css/bootstrap.css.map -> public/bootstrap/css/bootstrap.css.map 	renamed:    webpage/bootstrap/css/bootstrap.min.css -> public/bootstrap/css/bootstrap.min.css 	renamed:    webpage/bootstrap/css/bootstrap.min.css.map -> public/bootstrap/css/bootstrap.min.css.map 	renamed:    webpage/bootstrap/fonts/glyphicons-halflings-regular.eot -> public/bootstrap/fonts/glyphicons-halflings-regular.eot 	renamed:    webpage/bootstrap/fonts/glyphicons-halflings-regular.svg -> public/bootstrap/fonts/glyphicons-halflings-regular.svg 	renamed:    webpage/bootstrap/fonts/glyphicons-halflings-regular.ttf -> public/bootstrap/fonts/glyphicons-halflings-regular.ttf 	renamed:    webpage/bootstrap/fonts/glyphicons-halflings-regular.woff -> public/bootstrap/fonts/glyphicons-halflings-regular.woff 	renamed:    webpage/bootstrap/fonts/glyphicons-halflings-regular.woff2 -> public/bootstrap/fonts/glyphicons-halflings-regular.woff2 	renamed:    webpage/bootstrap/js/bootstrap.js -> public/bootstrap/js/bootstrap.js 	renamed:    webpage/bootstrap/js/bootstrap.min.js -> public/bootstrap/js/bootstrap.min.js 	renamed:    webpage/bootstrap/js/npm.js -> public/bootstrap/js/npm.js renamed:    webpage/css/stylesheet.css -> public/css/stylesheet.css 	renamed:    webpage/index.html -> public/index.html 	renamed:    webpage/jquery/jquery.js -> public/jquery/jquery.js 	renamed:    webpage/jquery/new_script.js -> public/jquery/new_script.js 	renamed:    webpage/jquery/script.js -> public/jquery/script.js 	renamed:    webpage/js/script.js -> public/js/script.js

--- index.js
+++ index.js
@@ -1,31 +1,44 @@
-var http = require('http');
-var fs = require('fs')
+var express = require('express');
+var app = express();
+var path = require('path')
+var server = require('http').createServer(app);
 
-var server = http.createServer(function(request, response) {
+server.listen(process.env.PORT || 8000);
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello Azure!");
+app.use(express.static(path.join(__dirname, 'public')));
 
-    // var pathname = url.parse(request.url).pathname;
-    //
-    // console.log('Request for ' + pathname + ' received.');
-    //
-    // fs.readFile(pathname.substr(1), function (err, data) {
-    //     if(err){
-    //         console.log(err);
-    //         response.writeHead(404, {'Content-Type': 'text/html'});
-    //     }
-    //     else {
-    //         response.writeHead(200, {'Content-Type': 'text/htl'});
-    //         response.write(data.toString());
-    //
-    //     }
-    //     response.end();
-    // });
-
+app.get('/', function(req, res){
+   res.sendFile(__dirname, 'index.html');
 });
 
-var port = process.env.PORT || 1337;
-server.listen(port);
-
-console.log("Server running at http://localhost:%d", port);
+// var http = require('http');
+// var fs = require('fs')
+// var url = require('url')
+//
+//
+// var server = http.createServer(function(request, response) {
+//
+//     var pathname = url.parse(request.url).pathname + 'views/index.html';
+//     console.log(pathname)
+//
+//     console.log('Request for ' + pathname + ' received.');
+//
+//     fs.readFile(pathname.substr(1), function (err, data) {
+//         if(err){
+//             console.log(err);
+//             response.writeHead(404, {'Content-Type': 'text/html'});
+//         }
+//         else {
+//             response.writeHead(200, {'Content-Type': 'text/html'});
+//             response.write(data.toString());
+//
+//         }
+//         response.end();
+//     });
+//
+// });
+//
+// var port = process.env.PORT || 1337;
+// server.listen(port);
+//
+// console.log("Server running at http://localhost:%d", port);
