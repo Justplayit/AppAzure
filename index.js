9f651a657457ad28375037c06330f612680cd732
Mihai Corciu <free_roaming94@yahoo.com>
2017-05-21 18:25:47 +0300

    modified:   .idea/workspace.xml 	modified:   index.js

--- index.js
+++ index.js
@@ -3,13 +3,20 @@ var app = express();
 var path = require('path')
 var server = require('http').createServer(app);
 
-server.listen(process.env.PORT || 1337);
+try {
+    server.listen(process.env.PORT || 1337);
 
-app.use(express.static(path.join(__dirname, 'public')));
+    app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, response){
-    response.sendFile(__dirname, 'index.html');
-});
+    app.get('/', function (request, response) {
+        if (request) {
+            console.log(request);
+        }
+        response.sendFile(__dirname, 'index.html');
+    });
+}catch(err){
+    console.log(err);
+}
 
 // var http = require('http');
 // var fs = require('fs')
