c561cb81643757a686bb02ff5743d65b91c064fe
Mihai Corciu <free_roaming94@yahoo.com>
2017-05-22 19:04:12 +0300

    modified html file

--- public/jquery/script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-
-    var modal = $('#upload-area');
-    // console.log(modal.height());
-    modal.css('margin-top', (window.innerHeight/2) - $('#upload').height());
-
-});
--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,7 @@
 
 $(document).ready(function() {
     var modal = $('#upload-area');
-    // console.log(modal.height());
     modal.css('margin-top', (window.innerHeight/2) - $('#upload').height());
-
 
     var $form = $('#form');
 
