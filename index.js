2a9f5953be4499f9070893362ee2e32c92291a50
Mihai Corciu <free_roaming94@yahoo.com>
2017-05-22 19:21:01 +0300

    modified index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var path = require('path')
 var server = require('http').createServer(app);
+var qs = require('querystring')
 
 try {
     server.listen(process.env.PORT || 1337);
@@ -21,18 +22,24 @@ try {
         }
         response.sendFile(__dirname, 'sitemap.xml');
     });
+
         app.get('/BingSiteAuth.xml', function (request, response) {
         if (request) {
             console.log(request);
         }
         response.sendFile(__dirname, 'BingSiteAuth.xml');
     });
+
       app.get('/sitemap', function (request, response) {
         if (request) {
             console.log(request);
         }
         response.sendFile(__dirname, 'sitemap.xml');
     });
+
+      app.post('/', function () {
+            console.log(post.file);
+      });
 }catch(err){
     console.log(err);
 }
