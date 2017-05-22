971895f3253ce99231419c64d679c1c9460b36c7
Mihai Corciu <free_roaming94@yahoo.com>
2017-05-22 20:10:35 +0300

    implemented file upload mechanism

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 var express = require('express');
 var app = express();
-var path = require('path')
+var path = require('path');
 var server = require('http').createServer(app);
-var qs = require('querystring')
+var bodyParser = require('body-parser');
+var formidable = require('formidable');
+var fs = require('fs');
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 try {
     server.listen(process.env.PORT || 1337);
 
     app.use(express.static(path.join(__dirname, 'public')));
-
-    console.log('something')
 
     app.get('/', function (request, response) {
         if (request) {
@@ -39,8 +41,25 @@ try {
         response.sendFile(__dirname, 'sitemap.xml');
     });
 
-      app.post('/', function () {
-            console.log(post.file);
+      app.post('/upload', function (request, response) {
+            var form = new formidable.IncomingForm();
+            form.multiples = true;
+            form.uploadDir = path.join(__dirname, '/uploads/');
+
+            form.on('file', function (field, file) {
+                fs.rename(file.path, path.join(form.uploadDir, file.name));
+            });
+
+            form.on('error', function (error) {
+               console.log('An error occurred: \n' + error);
+            });
+
+            form.on('end', function () {
+                response.end('success');
+            });
+
+            form.parse(request);
+
       });
 }catch(err){
     console.log(err);
--- public/js/script.js
+++ public/js/script.js
@@ -3,36 +3,56 @@ $(document).ready(function() {
     var modal = $('#upload-area');
     modal.css('margin-top', (window.innerHeight/2) - $('#upload').height());
 
-    var $form = $('#form');
+    $('#file-dialog').on('change', function () {
+       var files = $(this).get(0).files;
 
-    var ajaxData = new FormData($form.get(0));
-    console.log('here');
+       console.log(files);
 
-    $form.on('submit', function (event) {
-        $.ajax({
-            url: $form.attr('action'),
-            type: $form.attr('method'),
-            data: ajaxData,
-            dataType: 'json',
-            cache: false,
-            contentType: false,
-            processData: false,
-            complete: function (event) {
-                console.log('Finished...');
-                },
-            success: function (data) {
-                console.log('Success...');
-                },
-            error: function () {
-                console.log('Error...')
-            }
-            });
+       if (files.length > 0){
+           var formData = new FormData();
+           for (var index = 0; index < files.length; index += 1){
+               var file = files[index];
+
+               formData.append('files', file, file.name);
+           }
+
+           // console.log(formData.getAll('files'));
+
+           $.ajax({
+               url: '/upload',
+               type: 'POST',
+               data: formData,
+               processData: false,
+               contentType: false,
+               success: function (data) {
+                   console.log('upload successful!');
+               },
+               xhr: function() {
+                   var xhr = new XMLHttpRequest();
+                   xhr.upload.addEventListener('progress', function(event){
+                       var percentComplete = event.loaded / event.total;
+                       percentComplete = parseInt(percentComplete * 100);
+
+                       // update the Bootstrap progress bar with the new percentage
+                       // $('.progress-bar').text(percentComplete + '%');
+                       // $('.progress-bar').width(percentComplete + '%');
+
+                       // once the upload reaches 100%, set the progress bar text to done
+                       if (percentComplete === 100) {
+                           // $('.progress-bar').html('Done');
+                           console.log('Done');
+                       }
+                   }, false);
+
+                   return xhr;
+               }
+
+           });
+       }
     });
 
-    var $fileDialog = $('#file-dialog');
-    $fileDialog.on('change', function (event) {
-       $form.trigger('submit');
-    });
+
+
 
 });
 
