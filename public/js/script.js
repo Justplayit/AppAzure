c83dad8247f553adbbdad5f9b4d8205be4942d36
Mihai Corciu <free_roaming94@yahoo.com>
2017-05-22 19:00:35 +0300

    modified html file

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,29 @@
 
 $(document).ready(function() {
+    var $form = $('#form');
 
+    var ajaxData = new FormData($form.get(0));
+
+    $form.on('submit', function (event) {
+        $.ajax({
+            url: $form.attr('action'),
+            type: $form.attr('method'),
+            data: ajaxData,
+            dataType: 'json',
+            cache: false,
+            contentType: false,
+            processData: false,
+            complete: function (event) {
+                console.log('Finished...');
+                },
+            success: function (data) {
+                console.log('Success...');
+                },
+            error: function () {
+                console.log('Error...')
+            }
+            });
+    });
 
 });
 
