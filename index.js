58439bc04bfcf96c9e4e8c595581d7f06f5434d5
Mihai Corciu <free_roaming94@yahoo.com>
2017-05-28 12:04:06 +0300

    trying to upload image

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ var formidable = require('formidable');
 var fs = require('fs');
 var azure = require('azure-storage');
 
-const storageAccount = '';
-const storageAccessKey = '';
-const azureEndpoint = '';
-const containerName = '';
+const storageAccount = 'azureappb4';
+const storageAccessKey = 'M28H+AO14p6JiUDg6HeRQNPsYycCeLw4mMMSxNXSk5m5LjIt8jx11YULjXPDvGcgftjiGT089TDB3WLWhpkfJg==';
+const azureEndpoint = 'DefaultEndpointsProtocol=https;AccountName=azureappb4;AccountKey=M28H+AO14p6JiUDg6HeRQNPsYycCeLw4mMMSxNXSk5m5LjIt8jx11YULjXPDvGcgftjiGT089TDB3WLWhpkfJg==;EndpointSuffix=core.windows.net';
+const containerName = 'azureappb4';
+
+var blobClient = azure.createBlobService(storageAccount, storageAccessKey);
 
 try {
     server.listen(process.env.PORT || 1337);
@@ -36,9 +38,34 @@ try {
         }
         response.sendFile(__dirname, 'BingSiteAuth.xml');
     });
-    
-    app.post('/upload', function (response, request) {
 
+    var uploadFull = function(imgPath, itemId, options){
+        var fileName = itemId + '.jpg';
+        blobClient.createBlockBlogFromLocalFile(containerName, fileName, imgPath, options,
+            function(error){
+                if(error !== null){
+                    console.log('Azure full error: ', error);
+                }
+                else{
+                    console.log('Azure full success.');
+                }
+            });
+    };
+
+    app.post('/upload', function (response, request) {
+        var form = new formidable.IncomingForm();
+        var itemID = new Date().getTime();
+        form.parse(req, function(err, fields, files){
+           var options = {
+               contentType: 'image/jpeg',
+               metadata: {fileName: itemID}
+           };
+           var imgPath = files.image.path;
+           var machineID = fields.machineId;
+           var userName = fields.userName;
+
+           uploadFull(imgPath, itemID, options);
+        });
     });
 
 } catch(err){
